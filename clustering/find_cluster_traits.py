daf38e78b237381987d974fe548e5e1d3d76595e
Henry Carscadden <hlc5v@virginia.edu>
2021-04-17 16:16:30 -0400

    Added basic code for finding common cluster features

--- clustering/find_cluster_traits.py
+++ clustering/find_cluster_traits.py
@@ -1,10 +1,11 @@
 import sklearn
 
 
-def find_cluster_attributes(k_means_labels, all_items, item):
+def find_cluster_attributes(k_means_labels, all_items, item, features_use):
     """
 
     Args:
+        features_use: A list of features that will be used in the explanation
         k_means_labels: This is the second object returned by the k_means methods in sklearn, and provides
         the nearest centroid to the
         all_items: An ordered collection of all items in consideration in the same order as k_means_labels
@@ -13,5 +14,19 @@ def find_cluster_attributes(k_means_labels, all_items, item):
     Returns: Dictionary of the form {"item_feature": cluster_value} ( This allows us to flexibly change how many
     features we are looking at to explain the item's selection. )
     """
+
+    all_items["labels"] = k_means_labels
+
+    item_label = all_items.loc[all_items.id == item["id"], ["label"]]
+
+    common_cluster = all_items[all_items.label == item_label]
+
+    modes = {}
+    for feature in features_use:
+        # TODO: Add code to decode one hot encoded attributes.
+        modes["feature"] = common_cluster[feature].mode()
+
+    return modes
+
 
 
