52ea19926e6124a3a7328758c4ca759bbb2c96bc
Michael Chang <chang.michael1009@gmail.com>
2021-05-01 12:56:44 -0400

    Change file to use steam_reviews.json

--- clustering/train_clustering.py
+++ clustering/train_clustering.py
@@ -30,6 +30,7 @@ def build_dataframe(path, num_samples, categorical_cols, num_cols):
     """
     # Read data from file into Panda structure
     dataframe = pd.DataFrame(parse(path))
+    print(dataframe.head())
 
     # randomly select num_sample number of rows
     if num_samples != "MAX":
@@ -41,13 +42,13 @@ def build_dataframe(path, num_samples, categorical_cols, num_cols):
         dataframe[col] = pd.to_numeric(dataframe[col], errors="coerce").replace(np.NaN, 0)
 
     # Only keep selected columns
-    dataframe = dataframe[categorical_cols + num_cols + ["id"]].copy()
+    dataframe = dataframe[categorical_cols + num_cols].copy()
     print(dataframe.columns)
     print("Number of rows: " + str(len(dataframe.index)))
     print(dataframe.head())
 
     # Only keep selected columns
-    dataframe = dataframe[categorical_cols + num_cols + ["id"]].copy()
+    dataframe = dataframe[categorical_cols + num_cols].copy()
 
     # One hot encode categorical columns
     dataframe = pd.get_dummies(dataframe, columns=categorical_cols, prefix=categorical_cols)
@@ -69,30 +70,31 @@ def kmeans_fit(data_frame, min_cluster=2, max_cluster=10, min_improvement=.25):
     return k_means_fit
 
 
-# def get_centroids(k_means):
-#     kmeans = KMeans(n_clusters=2, random_state=0).fit(dataframe)
-#     centroids = kmeans.cluster_centers_
-#     print(centroids)
-#     return centroids
+def get_centroids(dataframe):
+    kmeans = KMeans(n_clusters=2, random_state=0).fit(dataframe)
+    centroids = kmeans.cluster_centers_
+    print(centroids)
+    return centroids
+
+
 def main():
-    # For steam_games.json.gz
-    path = "../data/steam_games.json.gz"
+    # For steam_reviews.json.gz
+    path = "../data/steam_reviews.json.gz"
     cat_cols = [
-        "publisher", "app_name", "title", "developer", "sentiment"
+        "username", "date", "text", "product_id"
     ]
-    num_cols = ["price"]
+    num_cols = ["hours", "products"]
     dataframe = build_dataframe(path, 10000, cat_cols, num_cols)
     dataframe.to_csv("clustering_examples.csv", index=False)
     # cat_cols_one_hot = list(filter(lambda x: x in dataframe.columns, cat_cols))
     # dataframe = dataframe[cat_cols_one_hot + num_cols]
-    dataframe.drop(labels=["id"], inplace=True, axis=1)
+    # dataframe.drop(labels=["id"], inplace=True, axis=1)
     print(dataframe.head())
 
+    # get_centroids(dataframe)
     sol = kmeans_fit(dataframe, min_improvement=.2)
     with open("kmeans_fit.bin", "wb") as fp:
         dump(sol, fp)
-
-
 
 
 if __name__ == "__main__":
